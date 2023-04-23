96578946076ff77aa38cb899978c632039789ee8
Nliker <tjwjdgus83@naver.com>
2023-04-23 22:38:20 +0900

    user_router 초기화

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,12 +1,28 @@
+import sys,os
 from fastapi import Depends,APIRouter
+from sql import get_db
+from sqlalchemy.orm import Session
+from sql import User,Product
+from schema import *
+sys.path.append((os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
+
 
 user_api=APIRouter(prefix="/users",tags=["users"])
 
 def user_router(app,services):
-    app.include_router(user_api)
+    user_service=services.user_service
+    product_service=services.product_service
     
+    app.include_router(user_api)
+
     @user_api.post("/sign_up")
-    async def post_sign_up(phone_number: str,password: str):
+    async def post_sign_up(new_user: UserCreate,db: Session = Depends(get_db)):
+        result=user_service.set_db(db).create_user(new_user)
+        print(result)
+
+        # result=db.query(User).filter(User.phone_number == phone_number).first()
+        # print(result.__dict__)
+
         return {"data":"good"}
 
     @user_api.post("/login")
