28a86419d2e15a71c9bed034e699f3491f9f4259
Nliker <tjwjdgus83@naver.com>
2023-04-24 01:35:43 +0900

    try except 에러 핸들링 구조 설계

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,11 +1,13 @@
 import sys,os
-from fastapi import Depends,APIRouter
+from fastapi import Depends,APIRouter,HTTPException
 from sql import get_db
 from sqlalchemy.orm import Session
 from sql import User,Product
 from schema import *
 from auth import verify_token
 import traceback
+import expection
+import inspect
 
 sys.path.append((os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 
@@ -19,23 +21,43 @@ def user_router(app,services):
     app.include_router(user_api)
 
     @user_api.post("/sign_up")
-    async def post_sign_up(new_user: UserCreate,db: Session = Depends(get_db)):
+    async def post_sign_up(new_user: PostSignUpUser,db: Session = Depends(get_db)):
         try:
-            result=user_service.set_db(db).create_user(new_user)
-            print(result)
+            # result=user_service.set_db(db).create_user(new_user)
+            print(5/0)
+            phone_number_existance=user_service.set_db(db).is_phone_number_existance(new_user.phone_number)
+            if phone_number_existance==False:
+                raise expection.PhoneNumberExists()
+            
+            is_user_deleted=user_service.set_db(db).is_user_deleted(new_user.phone_number)
+            if is_user_deleted==True:
+                raise expection.UserWasDeleted()
+            created_user=user_service.set_db(db).create_new_user()
 
             return {"data":"good"}
         except Exception as es:
-            print(traceback.format_exc())
-            return {"data":False}
+            if es.__class__.__name__ in inspect.getmembers(expection): 
+                expection.make_http_error(es)
+            else:
+                print(traceback.format_exc())
+                raise HTTPException(status_code=500, detail="Internel Server Error")
+
     @user_api.post("/login")
     async def post_login(phone_number: str,password: str):
-        return {"data":"good"}
+        try:
+        except Exception as es:
+            if es.__class__.__name__ in inspect.getmembers(expection): 
+                expection.make_http_error(es)
+            else:
+                print(traceback.format_exc())
+                raise HTTPException(status_code=500, detail="Internel Server Error")
     
     @user_api.get("{user_id}")
     async def get_user(user_id: int = Depends(verify_token),db: Session = Depends(get_db)):
         try:
-            print(user_id)
-            return {"data":"token_good"}
-        except:
-            return {"data":"token_failed"}
+        except Exception as es:
+            if es.__class__.__name__ in inspect.getmembers(expection): 
+                expection.make_http_error(es)
+            else:
+                print(traceback.format_exc())
+                raise HTTPException(status_code=500, detail="Internel Server Error")
