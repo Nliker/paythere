8c6080dfa8bee0421712e05c2efc9fe32aedfe62
Nliker <tjwjdgus83@naver.com>
2023-04-25 00:32:41 +0900

    PostProduct의 model 초본

--- app/model/product_model.py
+++ app/model/product_model.py
@@ -1,9 +1,41 @@
 import os,sys
-from sql import Product
-
+import sql
+from exception import DatabaseError
 sys.path.append((os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
+import traceback
+from schema import *
 
 class ProductModel:
+    def set_db(self,db):
+        self.db=db
 
-    def insert_user(self,new_Product,db):
-        return True
+    def insert_product(self,new_product: InsertProduct)->int:
+        """
+            새로운 상품의 정보를 삽입합니다.
+        """
+        try:
+            new_product=sql.Product(
+                **new_product.dict()
+            )
+            self.db.add(new_product)
+        except:
+            self.db.rollback()
+            raise DatabaseError()
+        else:
+            self.db.commit()
+            self.db.refresh(new_product)
+            new_product=Product(**new_product.__dict__)
+            return new_product
+        
+    def select_product_by_id(self,product_id: int)->Product:
+        """
+            상품의 번호와 일치하는 상품을 조회합니다.
+        """
+        try:
+            product=self.db.query(sql.Product).filter(sql.Product.id==product_id).first()
+        except:
+            raise DatabaseError()
+        else:
+            if product!=None:
+                product=Product(**product.__dict__)
+            return product
