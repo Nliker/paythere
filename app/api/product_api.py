79950abaca126c7b841ba7016356281916fb68af
Nliker <tjwjdgus83@naver.com>
2023-04-24 23:30:39 +0900

    product_api 초본작성

--- app/api/product_api.py
+++ app/api/product_api.py
@@ -1,14 +1,97 @@
-from fastapi import Depends,APIRouter
+import sys,os
+from fastapi import Depends,APIRouter,Response
+from sql import get_db
+from sqlalchemy.orm import Session
+from schema import *
+from auth import verify_token
+import traceback
+import exception
+from response import *
 
 product_api=APIRouter(prefix="/products",tags=["products"])
 
 def product_router(app,services):
+    user_service=services.user_service
+    product_service=services.product_service
+
     app.include_router(product_api)
 
-    @product_api.post("/")
-    async def post_product(category: str,net_price: str):
-        return {"data":"good"}
+    @product_api.post("/",status_code=Created_201.status_code,response_model=PostProductResponse)
+    async def post_product(response: Response,new_product:CreateProduct,current_user_id: int = Depends(verify_token),db: Session = Depends(get_db)):
+        """
+            상품을 생성합니다.
+        """
+        try:
+            return make_http_response_json(Created_201,{"product":"good"})
+        except Exception as es:
+                if exception.exceptions_dict.get(es.__class__.__name__,False):
+                    response.status_code=es.status_code
+                    return exception.make_http_error(es.status_code,es.__str__())
+                else:
+                    print(traceback.format_exc())
+                    response.status_code=500
+                    return exception.make_http_error(500,es.__str__())
 
-    @product_api.put("/{product_id}")
-    async def put_product(phone_number: str,password: str):
-        return {"data":"good"}
+    @product_api.put("/{product_id}",status_code=Created_201.status_code,response_model=PutProductResponse)
+    async def put_product(response: Response,update_product:UpdateProduct,current_user_id: int = Depends(verify_token),db: Session = Depends(get_db)):
+        """
+            상품의 정보를 업데이트 합니다.
+        """
+        try:
+            return make_http_response_json(Created_201,{"product":"good"})
+        except Exception as es:
+                if exception.exceptions_dict.get(es.__class__.__name__,False):
+                    response.status_code=es.status_code
+                    return exception.make_http_error(es.status_code,es.__str__())
+                else:
+                    print(traceback.format_exc())
+                    response.status_code=500
+                    return exception.make_http_error(500,es.__str__())
+    
+    @product_api.get("/{product_id}",status_code=Get_200.status_code,response_model=GetProductResponse)
+    async def get_product(response: Response,product_id: int,current_user_id: int = Depends(verify_token),db: Session = Depends(get_db)):
+        """
+            상품을 조회합니다.
+        """
+        try:
+            return make_http_response_json(Get_200,{"product":"good"})
+        except Exception as es:
+                if exception.exceptions_dict.get(es.__class__.__name__,False):
+                    response.status_code=es.status_code
+                    return exception.make_http_error(es.status_code,es.__str__())
+                else:
+                    print(traceback.format_exc())
+                    response.status_code=500
+                    return exception.make_http_error(500,es.__str__())
+    
+    @product_api.get("/",status_code=Get_200.status_code,response_model=GetProductsResponse)
+    async def get_products(response: Response,current_user_id: int = Depends(verify_token),db: Session = Depends(get_db)):
+        """
+            상품들을 조회합니다.
+        """
+        try:
+            return make_http_response_json(Get_200,{"product":"good"})
+        except Exception as es:
+                if exception.exceptions_dict.get(es.__class__.__name__,False):
+                    response.status_code=es.status_code
+                    return exception.make_http_error(es.status_code,es.__str__())
+                else:
+                    print(traceback.format_exc())
+                    response.status_code=500
+                    return exception.make_http_error(500,es.__str__())
+                
+    @product_api.delete("/{product_id}",status_code=Get_200.status_code,response_model=DeleteProductResponse)
+    async def delete_product(response: Response,product_id: int,current_user_id: int = Depends(verify_token),db: Session = Depends(get_db)):
+        """
+            상품을 삭제합니다.
+        """
+        try:
+            return make_http_response_json(Get_200,{"product":"good"})
+        except Exception as es:
+                if exception.exceptions_dict.get(es.__class__.__name__,False):
+                    response.status_code=es.status_code
+                    return exception.make_http_error(es.status_code,es.__str__())
+                else:
+                    print(traceback.format_exc())
+                    response.status_code=500
+                    return exception.make_http_error(500,es.__str__())
