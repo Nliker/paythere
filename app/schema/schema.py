112c65a47f0eab7a46f7b586921e036fc34383ed
Nliker <tjwjdgus83@naver.com>
2023-04-24 17:25:53 +0900

    updated_at None 타입 추가

---
+++ app/schema/schema.py
@@ -0,0 +1,33 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from datetime import datetime,date
+
+"""
+UserInfo->service에서 api로 나가는 데이터 형식
+User->model에서 불러온 데이터형식
+"""
+
+class UserBase(BaseModel):
+    phone_number: str
+
+class InsertUser(UserBase):
+    hashed_password: str
+
+class CreateUser(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    updated_at: datetime | None
+    created_at: datetime
+    deleted: datetime
+
+class UserInfo(UserBase):
+    id: int
+    created_at: datetime
+    class Config:
+        orm_mode = True
+        
+
+
+    
