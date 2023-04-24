730fb12ebdc3eed8583fdbf4aabafdfb47c9f4e3
Nliker <tjwjdgus83@naver.com>
2023-04-24 17:26:17 +0900

    user_api response 구조 변경

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,12 +1,12 @@
 import sys,os
-from fastapi import Depends,APIRouter,HTTPException
+from fastapi import Depends,APIRouter,HTTPException,Response
 from sql import get_db
 from sqlalchemy.orm import Session
 from sql import User,Product
 from schema import *
 from auth import verify_token
 import traceback
-import expection
+import exception
 import inspect
 from response import *
 
@@ -21,27 +21,29 @@ def user_router(app,services):
     
     app.include_router(user_api)
 
-    @user_api.post("/sign_up")
-    async def post_sign_up(new_user: PostSignUpUser,db: Session = Depends(get_db)):
+    @user_api.post("/sign_up",status_code=Created_201.status_code)
+    async def post_sign_up(response: Response,new_user: CreateUser,db: Session = Depends(get_db)):
         try:
             phone_number_existance=user_service.set_db(db).is_phone_number_existance(new_user.phone_number)
-            if phone_number_existance==False:
-                raise expection.PhoneNumberExists()
+            if phone_number_existance==True:
+                raise exception.PhoneNumberExists()
             
             is_user_deleted=user_service.set_db(db).is_user_deleted_by_phone_number(new_user.phone_number)
             if is_user_deleted==True:
-                raise expection.UserWasDeleted()
+                raise exception.UserWasDeleted()
 
             created_user_id=user_service.set_db(db).create_new_user(new_user)
             created_user_info=user_service.set_db(db).get_user_info_by_id(created_user_id)
 
             return make_http_response_json(Created_201,{"user":created_user_info})
         except Exception as es:
-            if es.__class__.__name__ in inspect.getmembers(expection): 
-                expection.make_http_error(es)
+            if exception.exceptions_dict.get(es.__class__.__name__,False):
+                response.status_code=es.status_code
+                return exception.make_http_error(es.status_code,es.__str__())
             else:
                 print(traceback.format_exc())
-                raise HTTPException(status_code=500, detail="Internel Server Error")
+                response.status_code=500
+                return exception.make_http_error(500,es.__str__())
 
     # @user_api.post("/login")
     # async def post_login(phone_number: str,password: str):
