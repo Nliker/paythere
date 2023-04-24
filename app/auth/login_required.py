7e5791fa115ecb77c2387ae99cfd73cdf543e725
Nliker <tjwjdgus83@naver.com>
2023-04-25 02:24:19 +0900

    로그인 디펜던시 에러 변경

--- app/auth/login_required.py
+++ app/auth/login_required.py
@@ -1,9 +1,10 @@
 import sys,os
 sys.path.append((os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
-from fastapi import Header
+from fastapi import Header,HTTPException
 from config import make_conf_dict
 import jwt
 from typing import Union
+import exception
 
 conf=make_conf_dict()
 
@@ -13,12 +14,14 @@ async def verify_token(access_token: Union  [str, None]=Header(default=None)):
             payload=jwt.decode(access_token,conf.jwt_secret_key,"HS256")
         except:
             print("유효하지 않은 토큰")
-            return {"data":False}
+            error=exception.BrokenToken()
+            raise HTTPException(status_code=error.status_code,detail=error.__str__())
         if 'user_id' not in payload and type(payload['user_id']) !=type(1):
             print("내용이 유효하지 않은 토큰")
-            return {"data":False}
+            error=exception.WrongToken()
+            raise HTTPException(status_code=error.status_code,detail=error.__str__())
     else:
-        print("토큰이 존재 안함")
-        return {"data":False}
+        error=exception.TokenNotExists()
+        raise HTTPException(status_code=error.status_code,detail=error.__str__())
     
     return payload['user_id']
