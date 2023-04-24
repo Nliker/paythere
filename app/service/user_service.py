18e9abe5e0d5fd93249fc94ffbb7717bb39f4391
Nliker <tjwjdgus83@naver.com>
2023-04-24 17:27:28 +0900

    user_service 유저 생성,유저 조회 초본 작성

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -2,7 +2,9 @@ import os,sys
 
 sys.path.append((os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 
-from expection import DatabaseError
+from exception import DatabaseError
+from schema import *
+import bcrypt
 
 class UserService:
     def __init__(self,user_model,conf):
@@ -13,11 +15,55 @@ class UserService:
         self.user_model.set_db(db)
         return self
 
-    def create_user(self,new_user):
+    def create_new_user(self,new_user: CreateUser)->int:
+        """
+            새로운 회원을 생성합니다.
+        """
         try:
-            print(5/0)
-            result=self.user_model.insert_user(new_user)
-            
-            return result
-        except DatabaseError as error_mode:
-            raise DatabaseError()
+            password=new_user.password
+            hashed_password=bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+            print(type(new_user))
+            inserted_user=self.user_model.insert_user(InsertUser(**new_user.dict(),hashed_password=hashed_password))
+    
+            return inserted_user.id
+        except DatabaseError as es:
+            raise es
+        
+    def is_phone_number_existance(self,phone_number: str)->bool:
+        """
+            핸드폰 번호의 회원이 등록되어 있는가를 확인합니다.
+        """
+        try:
+            user=self.user_model.select_user_by_phone_number(phone_number)
+            if user !=None:
+                return True
+            else:
+                return False
+        except DatabaseError as es:
+            raise es
+    
+    def is_user_deleted_by_phone_number(self,phone_number: str)->bool:
+        """
+            핸드폰 번호의 회원이 삭제된 기록이 있는 지를 확인합니다.
+        """
+        try:
+            user=self.user_model.select_user_by_phone_number(phone_number)
+            if user!=None and user.deleted==True:
+                return True
+            else:
+                return False
+        except DatabaseError as es:
+            raise es
+    
+    def get_user_info_by_id(self,user_id: int)->UserInfo:
+        """
+            유저의 번호를 통해 유저의 정보를 불러옵니다.
+        """
+        try:
+            user=self.user_model.select_user_by_id(user_id)
+            user=UserInfo(**user.dict())
+            return user
+        except DatabaseError as es:
+            raise es
+        
+        
