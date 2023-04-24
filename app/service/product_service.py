996d146a0485528400ed43acb992b34f5c84c0cd
Nliker <tjwjdgus83@naver.com>
2023-04-25 02:25:03 +0900

    PutProduct의 service 초본

--- app/service/product_service.py
+++ app/service/product_service.py
@@ -36,3 +36,50 @@ class ProductService:
         except DatabaseError as es:
             raise es
         
+    def is_product_id_exists(self,product_id: int)->bool:
+        """
+            상품의 번호를 통해 상품의 존재를 확인합니다.
+        """
+        try:
+            product=self.product_model.select_product_by_id(product_id)
+            if product!=None:
+                return True
+            else:
+                return False
+        except DatabaseError as es:
+            raise es
+
+    def is_product_deleted_by_id(self,product_id: int)->bool:
+        try:
+            product=self.product_model.select_product_by_id(product_id)
+            if product!=None and product.deleted==True:
+                return True
+        except DatabaseError as es:
+            raise es
+
+    def update_product_info_by_id(self,update_product: UpdateProduct,product_id: int)->UpdatedProductInfo:
+        """
+            상품의 번호와 일치하는 상품의 정보를 변경합니다.
+        """
+        try:
+            fitered_update_product=update_product.dict(exclude_unset=True)
+            print(fitered_update_product)
+            updated_count=self.product_model.update_product_by_id(fitered_update_product,product_id)
+            if updated_count==0:
+                return False
+            else:
+                updated_product=self.product_model.select_product_by_id(product_id)
+                updated_product_info=UpdatedProductInfo(**updated_product.dict())
+            return updated_product_info
+        except DatabaseError as es:
+            raise es
+        
+    def is_product_authorized_by_user_id(self,user_id: int,product_id: int)->int:
+        try:
+            product=self.product_model.select_product_by_id(product_id)
+            if product.user_id==user_id:
+                return True
+            else:
+                return False
+        except DatabaseError as es:
+            raise es
