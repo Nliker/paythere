18ab37badc8b2b7834e40e4bb87a9276da1f3375
Nliker <tjwjdgus83@naver.com>
2023-04-25 16:04:51 +0900

    __init__ 변경

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,9 @@
 from .user_api import user_router
 from .product_api import product_router
+from .search_api import search_router
 
 __all__=[
     "user_router",
-    "product_router"
+    "product_router",
+    "search_router"
 ]
