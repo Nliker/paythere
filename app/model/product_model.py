9093cecbdf2e68a48e39b6bd80ad362215b55dce
Nliker <tjwjdgus83@naver.com>
2023-04-25 16:05:37 +0900

    생성시 같이 동반되어야 하는 통신 트랜잭션 수정

--- app/model/product_model.py
+++ app/model/product_model.py
@@ -10,7 +10,7 @@ class ProductModel:
     def set_db(self,db):
         self.db=db
 
-    def insert_product(self,new_product: InsertProduct)->int:
+    def insert_product(self,new_product: InsertProduct,initial_name:str)->int:
         """
             새로운 상품의 정보를 삽입합니다.
         """
@@ -19,12 +19,22 @@ class ProductModel:
                 **new_product.dict()
             )
             self.db.add(new_product)
+            self.db.flush()
+            self.db.refresh(new_product)
+            
+            new_product_initial=sql.ProductInitial(
+                product_id=new_product.id,
+                initial=initial_name
+            )
+            self.db.add(new_product_initial)
         except:
+            print(traceback.format_exc())
             self.db.rollback()
             raise DatabaseError()
         else:
             self.db.commit()
             self.db.refresh(new_product)
+            self.db.refresh(new_product_initial)
             new_product=Product(**new_product.__dict__)
             return new_product
         
@@ -41,13 +51,18 @@ class ProductModel:
                 product=Product(**product.__dict__)
             return product
         
-    def update_product_by_id(self,fitered_update_product: dict,product_id: int)->int:
+    def update_product_by_id(self,fitered_update_product: dict,product_id: int,initial_name: str=False)->int:
         """
             상품의 번호와 일치하는 상품의 정보를 업데이트 합니다.
         """
         try:
             result=self.db.query(sql.Product).filter(sql.Product.id==product_id).update(fitered_update_product)
+            #실제 업데이트가 되었을 경우에만 실행
+            if initial_name!=False and result>=1:
+                result+=self.db.query(sql.ProductInitial).filter(sql.ProductInitial.product_id==product_id).update({"initial":initial_name})
         except:
+            print(traceback.format_exc())
+            self.db.rollback()
             raise DatabaseError()
         else:
             self.db.commit()
@@ -73,21 +88,24 @@ class ProductModel:
         try:
             like_query=f"%{name}%"
             product_list=self.db.query(sql.Product).filter(sql.Product.user_id==user_id,sql.Product.name.like(like_query)).all()
+            print(traceback.format_exc())
         except:
+            print(traceback.format_exc())
             raise DatabaseError()
         else:
             product_list=[Product(**product.__dict__) for product in product_list]
             return product_list
     
-    def select_product_by_user_id_with_name(self,initial: str,user_id: int)->List[Product]:
+    def select_product_by_user_id_with_initial(self,initial: str,user_id: int)->List[Product]:
         """
-            상풍의 
+            상풍의 초성이 포함된 상품들을 조회합니다.
         """
         try:
             like_query=f"%{initial}%"
-            product_list=self.db.query(sql.ProductInitial).filter(sql.ProductInitial.initial.like(like_query)).join(sql.Product.id).filter(sql.Product.id==user_id).all()
+            product_list=self.db.query(sql.ProductInitial,sql.Product).filter(sql.ProductInitial.initial.like(like_query)).join(sql.Product,sql.Product.id==sql.ProductInitial.product_id).all()
         except:
+            print(traceback.format_exc())
             raise DatabaseError()
         else:
-            product_list=[Product(**product.__dict__) for product in product_list]
+            product_list=[Product(**product.__dict__) for product_initial,product in product_list]
             return product_list
