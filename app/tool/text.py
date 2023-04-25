be353dba245b147c6cceca0072eabc94dba3a9fb
Nliker <tjwjdgus83@naver.com>
2023-04-25 11:57:28 +0900

    한글 분석 함수 추가

---
+++ app/tool/text.py
@@ -0,0 +1,36 @@
+def korean_check(words):
+    for ch in list(words.strip()):
+        if ch==" ":
+            continue
+        #한글 완전음인지 확인
+        if ch<'가' or ch>'힣':
+            print("완전음 아님")
+            return False
+
+    return True
+
+def korean_to_initial(korean_word):
+    """
+    한글 단어를 입력받아서 초성/중성/종성을 구분하여 리턴해줍니다. 
+    """
+    ####################################
+    # 초성 리스트. 00 ~ 18
+    CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    # 중성 리스트. 00 ~ 20
+    JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+    # 종성 리스트. 00 ~ 27 + 1(1개 없음)
+    JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    ####################################
+    r_lst = []
+    for w in list(korean_word.strip()):
+        if '가'<=w<='힣':
+            ch1 = (ord(w) - ord('가'))//588
+            ch2 = ((ord(w) - ord('가')) - (588*ch1)) // 28
+            ch3 = (ord(w) - ord('가')) - (588*ch1) - 28*ch2
+            r_lst.append([CHOSUNG_LIST[ch1], JUNGSUNG_LIST[ch2], JONGSUNG_LIST[ch3]])
+        else:
+            r_lst.append([w])
+    initial_list=[ch[0] for ch in r_lst]
+    return "".join(initial_list)
+
+
